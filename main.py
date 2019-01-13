1762f2aa0b7ebb2b6830b04f86a46bb0c35eb09f
wangsongiam <wangsongiam@gmail.com>
2019-01-12 17:01:05 -0800

    minor refactor

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
-import urllib.request
-import json
-import csv
-import datetime
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Song Wang"
+__email__ = "wangsongiam@gmail.com"
 
 # expireDate
 # http://stock.finance.sina.com.cn/futures/api/openapi.php/StockOptionService.getRemainderDay?date=201706
-# frontrow = [
+# FRONTROW = [
 #     'Date', 'ExpireDate', 'OptionType', 'Strike', 'Contract Name', 'Last',
 #     'Bid', 'Ask', 'Change', '%Change', 'Volume', 'OpenInterest',
 #     'ImpliedVolatility', 'UnderlyingPrice'
 # ]
 
-frontrow = [
+import urllib.request
+import json
+import csv
+import datetime
+FRONTROW = [
     'Date', '买量', '买价bid', '最新价last', '卖价ask', '卖量', '振幅%change', '涨跌幅change',
     '行权strike', '买量', '买价', '最新价', '卖价', '卖量', '振幅', '涨跌幅', '行权'
 ]
 
 
-def match_twins(month: int):
+def match_twins(month: int) -> list:
     prefix = 'http://hq.sinajs.cn/list=OP_'
     # suffix = '_51005017'
     suffix = '_510050'
     url1 = f'{prefix}UP{suffix}{str(month)}'
     url2 = f'{prefix}DOWN{suffix}{str(month)}'
-    return (get_paried_urls([url1, url2]))
+    return get_paried_urls([url1, url2])
 
 
 def get_paried_urls(twin_list: list) -> list:
@@ -66,17 +71,17 @@ def data_parser(double_query):
         params_seg = data[eq_pos + 2:-3]
         params = params_seg.split(',')
         row.extend(params[0:8])
-    return (row)
+    return row
 
 
 # url->
-def get_expire_url(month) -> str:
+def get_expire_url(month: str) -> str:
     prefixDate = 'http://stock.finance.sina.com.cn/futures/api/openapi.php/StockOptionService.getRemainderDay?date='
     url = f'{prefixDate}{str(month)}'
     return url
 
 
-def get_expire_date(url_link) -> str:
+def get_expire_date(url_link: str) -> str:
     with urllib.request.urlopen(url_link) as url:
         data = json.loads(url.read().decode())
         # print(data)
@@ -95,11 +100,11 @@ with open('sing_stock_data.csv', 'w', newline='') as csvfile:
         date = get_expire_date(get_expire_url(date_string[:6]))
 
         if len(match_twins(date_string[2:6])) == 0:
-            print(f'no data found in {date_string[4:6]} 月')
+            print(f'no data found in month {date_string[4:6]}')
         else:
             writer.writerow([f'{date_string[:6]}'])
-            print(f'found data from {date_string[4:6]} 月, start saving')
-            writer.writerow(frontrow)
+            print(f'found data from month {date_string[4:6]}, start saving')
+            writer.writerow(FRONTROW)
         for pairs in match_twins(date_string[2:6]):
             writer.writerow([date] + data_parser(pairs))
         writer.writerow([])
